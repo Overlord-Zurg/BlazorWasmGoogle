e0d943e2e5f23627a8226aa83efb2d8985c33e1b
Ben Lappin <Ben.S.Lappin@gmail.com>
2024-06-07 08:47:38 -0500

    add google auth details

--- Program.cs
+++ Program.cs
@@ -13,6 +13,13 @@ builder.Services.AddOidcAuthentication(options =>
     // Configure your authentication provider options here.
     // For more information, see https://aka.ms/blazor-standalone-auth
     builder.Configuration.Bind("Local", options.ProviderOptions);
+
+    options.ProviderOptions.Authority = "https://accounts.google.com/";
+    options.ProviderOptions.RedirectUri = "https://localhost:7167/authentication/login-callback";
+    options.ProviderOptions.PostLogoutRedirectUri = "https://localhost:7167/authentication/logout-callback";
+    options.ProviderOptions.ResponseType = "id_token";
+
+    options.ProviderOptions.ClientId = Environment.GetEnvironmentVariable("GOOGLE_CLIENT_ID");
 });
 
 await builder.Build().RunAsync();
